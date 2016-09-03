37f3d22d92a1ac1770780be656debcbfc4e32fb4
Makis <Nazgulius@yandex.ru>
2016-09-03 18:20:17 +0300

    Initial commit

---
+++ app/src/main/java/makis/myapptwo/ui/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package makis.myapptwo.ui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import makis.myapptwo.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
